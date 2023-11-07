5f717960bf4e74667ac0f4c9c66b9207e53292a0
Codensity <codensity30@gmail.com>
2023-11-07 16:51:18 +0530

    authentication added

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,14 +1,18 @@
 import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
+import { InvoiceForm, CustomerField } from "@/app/lib/definitions";
+import { notFound } from "next/navigation";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
 
-  const [invoice, customers] = await Promise.all([
-    fetchInvoiceById(id),
-    fetchCustomers(),
-  ]);
+  const [invoice, customers]: [InvoiceForm | undefined, CustomerField[]] =
+    await Promise.all([fetchInvoiceById(id), fetchCustomers()]);
+
+  if (!invoice) {
+    notFound();
+  }
 
   return (
     <main>
---
+++ app/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "@/app/ui/login-form";
+
+export default function Page() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div className="w-96">
+        <LoginForm />
+      </div>
+    </div>
+  );
+}
